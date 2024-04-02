f132074ea6af0282f9a2365153fd10e2412063be
Aztarok <Aztarok2994@gmail.com>
2024-04-02 15:47:32 -0400

    Setup tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '@/prismicio';
+import './globals.css';
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className="">{children}</body>
+			<PrismicPreview repositoryName={repositoryName} />
+		</html>
+	);
 }
